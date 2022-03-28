6ce606022da8cff2997b3e83886d3efdc6d0cad2
Eduardo <eduardo.baez@mercadolibre.cl>
2022-03-28 15:38:43 -0300

    feat: added main tab bar + initial home screen and initial favorites screen

---
+++ TopTwenty/Utils/NavigationStyleHelper.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class NavigationStyleHelper {
+    let navigationController: UINavigationController?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func setNavigationBarStyle() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(named: "primary")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black, .font: UIFont.init(name: "HelveticaNeue", size: 18)!]
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+    }
+}
